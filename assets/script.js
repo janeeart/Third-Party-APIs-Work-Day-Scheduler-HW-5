be6bd85c3de3f1996d63cf57aebf5efc8cbc7e2f
Janée Artis <terri.artis@du.edu>
2021-03-24 09:52:19 -0600

    Saved events persist after refresh

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,7 @@ for (i = 9; i <= 17; i++) {
     var newRow = $("<div>").attr("class", "row time-block")
 
     var newHour = $("<div>").attr("class", "col-2").text(timeText[i - 9])
-    var newTA = $("<textarea>").attr("class", "textArea").attr("id", "inputs")
+    var newTA = $("<textarea>").attr("class", "textArea").attr("id", timeText[i - 9])
     if (currentHour > i) {
         newTA.attr("class", "past col-8")
     } else if (currentHour == i) {
@@ -30,7 +30,7 @@ function saveToLocal() {
 
 
     var timeBlock = $(this).siblings(".col-2").text();
-    var savedData = JSON.parse(localStorage.getItem("currentHour") || "[]");
+    var savedData = JSON.parse(localStorage.getItem(timeText[i - 9]) || "[]");
     var storageObject = $(this).prev().val();
 
     savedData.push(storageObject);
@@ -39,13 +39,14 @@ function saveToLocal() {
 
 $(document).on("click", "button", saveToLocal)
 
-$("#9AM .textArea").text(localStorage.getItem("9AM"))
-$("#10AM .textArea").text(localStorage.getItem("10AM"))
-$("#11AM .textArea").text(localStorage.getItem("11AM"))
-$("#12PM .textArea").text(localStorage.getItem("12PM"))
-$("#1PM .textArea").text(localStorage.getItem("1PM"))
-$("#2PM .textArea").text(localStorage.getItem("2PM"))
-$("#3PM .textArea").text(localStorage.getItem("3PM"))
-$("#4PM .textArea").text(localStorage.getItem("4PM"))
-$("#5PM .textArea").text(localStorage.getItem("5PM"))
+
+$("#9AM").text(localStorage.getItem("9AM"))
+$("#10AM").text(localStorage.getItem("10AM"))
+$("#11AM").text(localStorage.getItem("11AM"))
+$("#12PM").text(localStorage.getItem("12PM"))
+$("#1PM").text(localStorage.getItem("1PM"))
+$("#2PM").text(localStorage.getItem("2PM"))
+$("#3PM").text(localStorage.getItem("3PM"))
+$("#4PM").text(localStorage.getItem("4PM"))
+$("#5PM").text(localStorage.getItem("5PM"))
 
