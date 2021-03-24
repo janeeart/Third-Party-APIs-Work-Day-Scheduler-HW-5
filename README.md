81aceb7d4b44ffbeb1ee4d303d864ed37fa43b64
Janée Artis <terri.artis@du.edu>
2021-03-23 22:51:05 -0600

    Values and keys saving properly

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Third-Party-APIs-Work-Day-Scheduler-HW-5
+
+This is a simple calendar application that allows a user to sae events for each hour of the day. This application was built by modifying starter code. 
+
+## Description 
+
+This app runs in the browswer and features dynamically updated HTML and CSS powered by jQuery. Moment.js library was used to work with the date and time at the top of the page. The purpose of the application was to enable employees with busy schedules to add important events to a daily planner so that their time can be managed effectively. 
--- assets/script.js
+++ assets/script.js
@@ -3,20 +3,20 @@ $("#currentDay").html(todayDate);
 
 var currentHour = moment().format("HH")
 
-var timeText= ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
-for (i=9; i<=17; i++){
+var timeText = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
+for (i = 9; i <= 17; i++) {
     var newRow = $("<div>").attr("class", "row time-block")
 
-    var newHour = $("<div>").attr("class", "col-2").text(timeText[i-9])
-    var newTA = $("<textarea>")
-    if(currentHour > i) {
+    var newHour = $("<div>").attr("class", "col-2").text(timeText[i - 9])
+    var newTA = $("<textarea>").attr("class", "textArea").attr("id", "inputs")
+    if (currentHour > i) {
         newTA.attr("class", "past col-8")
-    } else if (currentHour == i){
+    } else if (currentHour == i) {
         newTA.attr("class", "present col-8")
     } else {
         newTA.attr("class", "future col-8")
     }
-    
+
     var newBtn = $("<button>").attr("class", "saveBtn col-2").text("Save")
 
     newRow.append(newHour)
@@ -26,16 +26,26 @@ for (i=9; i<=17; i++){
     $(".container").append(newRow)
 }
 
-function saveToLocal(){
+function saveToLocal() {
 
 
-    var savedData = JSON.parse(localStorage.getItem("currentHour")) || [];
-    var storageObject = {
-    text : $(this).prev().val(),
-    time : $(this).timeText([i-9])
-    }
-savedData.push(storageObject);
-       localStorage.setItem("currentHour", JSON.stringify(savedData));
-    }
+    var timeBlock = $(this).siblings(".col-2").text();
+    var savedData = JSON.parse(localStorage.getItem("currentHour") || "[]");
+    var storageObject = $(this).prev().val();
+
+    savedData.push(storageObject);
+    localStorage.setItem(timeBlock, storageObject)
+}
 
 $(document).on("click", "button", saveToLocal)
+
+$("#9AM .textArea").text(localStorage.getItem("9AM"))
+$("#10AM .textArea").text(localStorage.getItem("10AM"))
+$("#11AM .textArea").text(localStorage.getItem("11AM"))
+$("#12PM .textArea").text(localStorage.getItem("12PM"))
+$("#1PM .textArea").text(localStorage.getItem("1PM"))
+$("#2PM .textArea").text(localStorage.getItem("2PM"))
+$("#3PM .textArea").text(localStorage.getItem("3PM"))
+$("#4PM .textArea").text(localStorage.getItem("4PM"))
+$("#5PM .textArea").text(localStorage.getItem("5PM"))
+
